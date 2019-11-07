c7a8c21f0ca8ff326d65cc9c32969762ae85d9ab
Lea Belzunces <lcbelzunces@gmail.com>
2019-11-07 21:35:10 +0100

    app construction ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_list = open(url).read
+drinks = JSON.parse(drinks_list)
+drinks["drinks"].each do |drink|
+  ingredient = drink["strIngredient1"]
+  Ingredient.create(name: ingredient)
+end
