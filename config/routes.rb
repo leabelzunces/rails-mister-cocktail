dbd9dad5995df63398355894db42e9dda5490623
Lea Belzunces <lcbelzunces@gmail.com>
2019-11-08 12:18:36 +0100

    get / routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
-  root to: 'cocktails#index'
+  get '/', to: "cocktails#index"
 end
