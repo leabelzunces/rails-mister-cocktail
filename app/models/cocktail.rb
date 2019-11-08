75b34c6039c6c3e29ed3bdf989fe78eb6c6537f3
Lea Belzunces <lcbelzunces@gmail.com>
2019-11-08 17:21:41 +0100

    cards

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,14 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
 
   validates :name, presence: :true, uniqueness: :true
+
+  mount_uploader :picture, PhotoUploader
+
+  def back_image
+    if picture.present?
+      picture
+    else
+      'https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1586&q=80'
+    end
+  end
 end
